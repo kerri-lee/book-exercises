2dd6bc402398be430173e8e92f735bae7f628fe9
kerri <kerril2k@cs.washington.edu>
2020-02-13 15:13:10 -0800

    added week 6 exercises

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -4,13 +4,12 @@ library("jsonlite")
 
 # Be sure and check the README.md for complete instructions!
 
-
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikey.R")
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "1917"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,26 +19,43 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+resource <- "reviews/search.json"
+
+query_params <- list("api-key" = nyt_key, query = movie_name)
 
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response <- GET(paste0(base_uri, resource), query = query_params)
+response
+body <- fromJSON(content(response, "text", encoding = "UTF-8"))
 
 # What kind of data structure did this produce? A data frame? A list?
-
+class(body)
+is.list(body) # TRUE
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+names(body)
+names(body$results)
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten(body$results)
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+most_recent_review <- reviews[1, ]
+headline <- most_recent_review$headline
+short_summary <- most_recent_review$summary_short
+link <- most_recent_review$link.url
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+review <- list(
+  headline=headline,
+  short_summary=short_summary,
+  link=link
+)
+print(review)
